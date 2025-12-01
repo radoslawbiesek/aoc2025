7763fd4ef58278f31f4190f0c8173f4199230527
Radosław Biesek <radoslaw.biesek@gmail.com>
2025-12-01 15:50:55 +0100

    day01 optimizations

--- src/day01/main.test.ts
+++ src/day01/main.test.ts
@@ -1,14 +1,26 @@
-import { test } from "node:test";
+import { test, describe } from "node:test";
 import * as assert from "node:assert";
 
 import { part1, part2 } from "./main.ts";
 
-test("day01 - part1", () => {
-  assert.equal(part1("test-input.txt"), 3);
-  assert.equal(part1("input.txt"), 1182);
-});
+describe("day01", () => {
+  describe("part1", () => {
+    test("test input", () => {
+      assert.equal(part1("test-input.txt"), 3);
+    });
 
-test("day01 - part2", () => {
-  assert.equal(part2("test-input.txt"), 6);
-  assert.equal(part2("input.txt"), 6907);
+    test("input", () => {
+      assert.equal(part1("input.txt"), 1182);
+    });
+  });
+
+  describe("part2", () => {
+    test("test input", () => {
+      assert.equal(part2("test-input.txt"), 6);
+    });
+
+    test("input", () => {
+      assert.equal(part2("input.txt"), 6907);
+    });
+  });
 });
--- src/day01/main.ts
+++ src/day01/main.ts
@@ -1,58 +1,49 @@
 import { readInput } from "../utils.ts";
 
 class Dial {
-  readonly start = 0;
-  readonly end = 99;
-  position = 50;
-  startCount = 0;
+  public readonly start = 0;
+  public readonly end = 99;
+  public position = 50;
+  public startCount = 0;
 
-  rotate(direction: "L" | "R", distance: number): void {
-    let distanceLeft = distance;
+  private parseInstruction(instruction: string) {
+    const direction = instruction[0];
+    if (direction !== "L" && direction !== "R") {
+      throw new Error(`Invalid direction: ${direction}`);
+    }
+    const distance = parseInt(instruction.slice(1));
 
-    while (distanceLeft > 0) {
-      if (direction === "L") {
-        this.position--;
-        if (this.position < this.start) {
-          this.position = this.end;
-        }
-      } else {
-        this.position++;
-        if (this.position > this.end) {
-          this.position = this.start;
-        }
+    return { direction, distance };
+  }
+
+  public rotate(instruction: string): void {
+    const { direction, distance } = this.parseInstruction(instruction);
+    const rangeLength = this.end - this.start + 1;
+
+    if (direction === "R") {
+      const diff = this.position + distance;
+      this.position = diff % rangeLength;
+      this.startCount += Math.floor(diff / rangeLength);
+    } else {
+      const startPos = this.position;
+      const diff = this.position - distance;
+      this.position = (rangeLength + (diff % rangeLength)) % rangeLength;
+      if (diff <= 0) {
+        this.startCount +=
+          Math.floor(Math.abs(diff / rangeLength)) +
+          (startPos === this.start ? 0 : 1);
       }
-      if (this.position === this.start) {
-        this.startCount++;
-      }
-
-      distanceLeft--;
     }
   }
 }
 
-function parseLine(line: string): { direction: "R" | "L"; distance: number } {
-  const direction = line[0];
-  if (direction !== "L" && direction !== "R") {
-    throw new Error(`Invalid direction: ${direction}`);
-  }
-  const distance = parseInt(line.slice(1));
-
-  return { direction, distance };
-}
-
-function getLines(filename: string) {
-  const input = readInput(filename);
-
-  return input.map(parseLine);
-}
-
-export function part1(path: string) {
-  const lines = getLines(path);
+export function part1(filename: string) {
+  const instructions = readInput(filename);
   const dial = new Dial();
   let startCount = 0;
 
-  for (const { direction, distance } of lines) {
-    dial.rotate(direction, distance);
+  for (const instruction of instructions) {
+    dial.rotate(instruction);
     if (dial.position === dial.start) {
       startCount++;
     }
@@ -62,11 +53,11 @@ export function part1(path: string) {
 }
 
 export function part2(filename: string) {
-  const lines = getLines(filename);
+  const instructions = readInput(filename);
   const dial = new Dial();
 
-  for (const { direction, distance } of lines) {
-    dial.rotate(direction, distance);
+  for (const instruction of instructions) {
+    dial.rotate(instruction);
   }
 
   return dial.startCount;
